41a7e8149df4cfd47dfdb67ea2404ee869aba23e
tarek <tarek.seba@outlook.fr>
2022-05-02 17:49:44 +0200

    Initialisation

---
+++ src/components/Root.jsx
@@ -0,0 +1,15 @@
+const Root = () => {
+  return (
+    <div>
+      <span>/</span>
+      <ul>
+        <li>File 1</li>
+        <li>File 1</li>
+        <li>File 1</li>
+        <li>File 1</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Root;
